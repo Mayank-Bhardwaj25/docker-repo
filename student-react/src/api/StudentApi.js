97a10938066e446d14e0218ee1b9cc73ef092f35
Mayank-Bhardwaj25 <Mayank.2.Bhardwaj@coforge.com>
2026-04-17 06:38:27 +0000

    Add frontend docker configs

--- student-react/src/api/StudentApi.js
+++ student-react/src/api/StudentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:8080/students"
+    baseURL:"https://scaling-garbanzo-jj66gqxr469jc5wrw-8082.app.github.dev/students"
 })
 export default api
