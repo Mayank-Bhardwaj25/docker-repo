9ae960d43ae6f39f89e30c1e5ec2eab7c244bbc5
Mayank-Bhardwaj25 <Mayank.2.Bhardwaj@coforge.com>
2026-04-17 04:41:07 +0000

    Docker Configs

--- Day23/src/main/java/com/coforge/controller/UserController.java
+++ Day23/src/main/java/com/coforge/controller/UserController.java
@@ -2,8 +2,6 @@ package com.coforge.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,26 +17,25 @@ import com.coforge.services.UserService;
 @RequestMapping("/user")
 public class UserController {
 
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);	
 	
 	@Autowired
 	UserService userservice;
 	@GetMapping("/")
 	public List<User> getAllUser() {
-		logger.info("Fetching all the users");
+		
 		return userservice.getAllUsers();
 	}
 	
 	@PostMapping("/")
 	public User saveUser(@RequestBody User user) {
-		logger.info("Saving the current User");
+		
 		return userservice.saveUser(user);
 	}
 	
 	
 	@GetMapping("/{id}")
 	public User findUserById(@PathVariable("id") long id) {
-		logger.info("Getting this id user");
+		
 		return userservice.FindById(id);
 	}
 }
